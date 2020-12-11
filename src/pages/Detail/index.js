1581c61951e911a95de6a635b80d5d0c8e956e38
Joao Paulo Valadares <joaopaulobsb12345@gmail.com>
2020-12-11 12:10:37 -0300

    Mobile 1.0

---
+++ src/pages/Detail/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { TouchableOpacity, View } from 'react-native';
+
+import styles from './styles.js';
+import logoImg from '../../assets/logo.png';
+
+export default function Detail(){
+    return (
+        <View style={styles.container}>
+          <View style={styles.header}>
+             <Image source={logoImg}/>
+             
+             <TouchableOpacity onPress={() => {}}>
+                 <Feather name="arrow-left" size={28} color="#E82041" />
+             </TouchableOpacity>
+          </View> 
+        </View>
+    );
+}
